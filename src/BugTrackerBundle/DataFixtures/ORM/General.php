f1535f276d2f1376f146a8a233a9fcb106c8ec7c
Sergii Miasnoi <smiasnoi@oroinc.com>
2017-09-17 21:56:21 +0300

    fixture writting

--- src/BugTrackerBundle/DataFixtures/ORM/General.php
+++ src/BugTrackerBundle/DataFixtures/ORM/General.php
@@ -1,46 +1,39 @@
 <?php
+namespace BugTrackerBundle\DataFixtures\ORM;
 
-namespace BugTrackerBundle\Controller;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use BugTrackerBundle\Entity\User as UserEntity;
+use BugTrackerBundle\Entity\Issue as IssueEntity;
+use BugTrackerBundle\Entity\Project as ProjectEntity;
+use BugTrackerBundle\Entity\Activity as ActivityEntity;
+use BugTrackerBundle\Entity\Comment as CommentEntity;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
-use BugTrackerBundle\Entity;
-
-class FixtureController extends Controller
+class General extends Fixture
 {
-    /**
-     * Fixture route to populate bug tracker data
-     * (Doctrine entities relations checks)
-     *
-     * @Route("/fixture", name="bug_tracker_fixture")
-     * @Method({"GET"})
-     */
-    public function generateDataAction()
+    public function load(ObjectManager $manager)
     {
-        $issueTypes = array_keys(Entity\Issue::getTypes());
-        $issueStatuses = array_keys(Entity\Issue::getStatuses());
-        $issuePriorities = array_keys(Entity\Issue::getPriorities());
-        $issueResolutions = array_keys(Entity\Issue::getResolutions());
-        $userRoles = [Entity\User::ADMIN_ROLE, Entity\User::MANAGER_ROLE, Entity\User::OPERATOR_ROLE];
+        $issueHelper = $this->container->get('bugtracker.issue.helper');
+        $issueTypes = array_keys($issueHelper->getAllTypes());
+        $issueStatuses = array_keys($issueHelper->getAllStatuses());
+        $issuePriorities = array_keys($issueHelper->getPriorities());
+        $issueResolutions = array_keys($issueHelper->getResolutions());
+        $userRoles = [UserEntity::ADMIN_ROLE, UserEntity::MANAGER_ROLE, UserEntity::OPERATOR_ROLE];
 
-        $em = $this->getDoctrine()->getManager();
         $encoder = $this->container->get('security.password_encoder');
-
         $parentIssue = null;
         $plainPassword = 'qwerty123';
 
-        $project = new Entity\Project();
+        $project = new ProjectEntity();
         $project->setSummary('Project summary. Content: ' . uniqid())
             ->setLabel('Label-' . uniqid())
             ->setCode('PACD-' . uniqid());
-        $em->persist($project);
-        $em->flush();
+        $manager->persist($project);
+        $manager->flush();
 
         $users = [];
         for ($j = 0; $j < 15; $j++) {
-            $user = new Entity\User();
+            $user = new UserEntity();
             $encoded = $encoder->encodePassword($user, $plainPassword);
             $userRole = $userRoles[rand(0, count($userRoles) - 1)];
             $user->setEmail(uniqid() . '@oroinc.com')
@@ -48,15 +41,15 @@ class FixtureController extends Controller
                 ->setFullname(uniqid() . 'Some Doe')
                 ->setPassword($encoded)
                 ->setRoles([$userRole]);
-            $em->persist($user);
-            $em->flush();
+            $manager->persist($user);
+            $manager->flush();
 
             $users[] = $user;
 
             // adding issues
             $issuesAmmount = rand(1, 3);
             for ($k = 0; $k < $issuesAmmount; $k++) {
-                $issue = new Entity\Issue();
+                $issue = new IssueEntity();
                 $issueType = $issueTypes[rand(0, count($issueTypes) - 1)];
                 $issueStatuse = $issueStatuses[rand(0, count($issueStatuses) - 1)];
                 $issuePriority = $issuePriorities[rand(0, count($issuePriorities) - 1)];
@@ -85,21 +78,21 @@ class FixtureController extends Controller
                 if ($issueType == 'subtask') {
                     $issue->setParent($parentIssue);
                 }
-                $em->persist($issue);
-                $em->flush();
+                $manager->persist($issue);
+                $manager->flush();
 
                 // adding 'new issue' activity
-                $activity = new Entity\Activity();
+                $activity = new ActivityEntity();
                 $snappedData = ['issue_code' => $issue->getCode()];
                 $activity->setProject($issue->getProject())
                     ->setUser($user)
                     ->setEntityId($issue->getId())
                     ->setEntity('Issue')
                     ->setSnappedData($snappedData)
-                    ->setType(Entity\Activity::NEW_ISSUE_TYPE)
+                    ->setType(ActivityEntity::NEW_ISSUE_TYPE)
                     ->setCreatedAt(new \DateTime());
-                $em->persist($activity);
-                $em->flush();
+                $manager->persist($activity);
+                $manager->flush();
 
                 if ($issue->getStatus() != 'new') {
                     $snappedData = [
@@ -107,33 +100,33 @@ class FixtureController extends Controller
                         'old_status' => 'new',
                         'status' => $issue->getStatus()
                     ];
-                    $activity = new Entity\Activity();
+                    $activity = new ActivityEntity();
                     $activity->setIssue($issue)
                         ->setProject($issue->getProject())
                         ->setUser($user)
                         ->setEntityId($issue->getId())
                         ->setEntity('Issue')
                         ->setSnappedData($snappedData)
-                        ->setType(Entity\Activity::ISSUE_STATUS_CHANGE_TYPE)
+                        ->setType(ActivityEntity::ISSUE_STATUS_CHANGE_TYPE)
                         ->setCreatedAt(new \DateTime());
-                    $em->persist($activity);
-                    $em->flush();
+                    $manager->persist($activity);
+                    $manager->flush();
                 }
 
                 //adding comments
                 $commentsAmmount = rand(1, 3);
                 for ($k = 0; $k < $commentsAmmount; $k++) {
                     $commentator = $users[rand(0, count($users) - 1)];
-                    $comment = new Entity\Comment();
+                    $comment = new CommentEntity();
                     $comment->setBody('Unique comment for user ' . $commentator->getFullname())
                         ->setAuthor($commentator)
                         ->setIssue($issue)
                         ->setCreatedAt(new \DateTime());
-                    $em->persist($comment);
-                    $em->flush();
+                    $manager->persist($comment);
+                    $manager->flush();
 
                     // add 'post comment' issue activity
-                    $activity = new Entity\Activity();
+                    $activity = new ActivityEntity();
                     $snappedData = [
                         'issue_code' => $issue->getCode(),
                         'comment_body' => $comment->getBody()
@@ -145,21 +138,18 @@ class FixtureController extends Controller
                         ->setEntityId($comment->getId())
                         ->setEntity('Comment')
                         ->setSnappedData($snappedData)
-                        ->setType(Entity\Activity::COMMENT_POST_TYPE)
+                        ->setType(ActivityEntity::COMMENT_POST_TYPE)
                         ->setCreatedAt(new \DateTime());
-                    $em->persist($activity);
-                    $em->flush();
+                    $manager->persist($activity);
+                    $manager->flush();
                 }
             }
-            $em->flush();
+            $manager->flush();
         }
 
         foreach ($users as $user) {
             $project->addMember($user);
         }
-        $em->flush();
-
-        echo "Test data has been generated";
-        exit;
+        $manager->flush();
     }
 }
